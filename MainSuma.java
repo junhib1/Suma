8b6abfe199bdcd3de2e8d687035278cfb23911c9
Mairim <nygmaurie@gmail.com>
2024-04-24 21:57:44 -0600

    version 4

---
+++ MainSuma.java
@@ -0,0 +1,9 @@
+public class MainSuma{
+    public static void main(String[] args) {
+        Suma suma = new Suma();
+
+        suma.sumarA();
+        suma.sumarB();
+        suma.mostrarResultado();
+    }
+}
---
+++ Suma.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Suma {
+    int a, b;
+
+    Suma() {
+        System.out.println("SE ESTA CONSTRUYENDO LA SUMA");
+    }
+
+    public void sumarA() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa el primer numero: ");
+        a = scanner.nextInt();
+    }
+
+    public void sumarB() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa el segundo numero: ");
+        b = scanner.nextInt();
+    }
+
+    public void mostrarResultado() {
+        System.out.println("El resultado es: " + (a + b));
+    }
+}
